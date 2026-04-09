ae99924740ba7c6f996a6f4aa41dee36b0a8ca29
denis kukulenko <kukulenko.denys@gmail.com>
2026-04-09 14:40:41 +0300

    Update code-examples-pract1.cs

--- Pract1/pzpi-23-8-kukulenko-denys-pract1/code-examples-pract1.cs
+++ Pract1/pzpi-23-8-kukulenko-denys-pract1/code-examples-pract1.cs
@@ -1,48 +1,39 @@
 using System;
 
-// Інтерфейс команди — визначає єдиний метод Execute()
 public interface ICommand
 {
     void Execute();
 }
 
-// Receiver — клас, який виконує реальну бізнес-логіку
 public class Light
 {
-    // Метод увімкнення світла
     public void TurnOn()
     {
         Console.WriteLine("Світло увімкнено");
     }
 }
 
-// ConcreteCommand — конкретна команда
-// Реалізує інтерфейс ICommand
+
 public class TurnOnLightCommand : ICommand
 {
-    // Посилання на Receiver 
     private Light _light;
 
-    // Конструктор приймає Receiver 
     public TurnOnLightCommand(Light light)
     {
         _light = light;
     }
 
-    // Метод Execute викликається Invoker'ом
     public void Execute()
     {
         _light.TurnOn();
     }
 }
 
-// Invoker — клас, який ініціює виконання команди
+
 class RemoteControl
 {
-    // Посилання на команду через інтерфейс
     private ICommand _command;
 
-    // Метод для встановлення команди
     public void SetCommand(ICommand command)
     {
         _command = command;
@@ -55,24 +46,18 @@ class RemoteControl
     }
 }
 
-// Client — точка входу в програму
 class Program
 {
     static void Main()
     {
-        // Створюємо Receiver
         Light light = new Light();
 
-        // Створюємо команду і передаємо в неї Receiver
         ICommand command = new TurnOnLightCommand(light);
 
-        // Створюємо Invoker
         RemoteControl remote = new RemoteControl();
 
-        // Передаємо команду в Invoker
         remote.SetCommand(command);
 
-        // Викликаємо команду 
         remote.PressButton();
     }
 }
